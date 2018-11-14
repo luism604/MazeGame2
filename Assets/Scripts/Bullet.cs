d47248d402d1cc33630ef440aa6e730374da34d4
Luis <luism@tlu.ee>
2018-11-14 19:57:18 +0200

    november14

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,18 +8,31 @@ public class Bullet : MonoBehaviour {
 	public float moveSpeed = 7.0f;
 
 	private float timePassed = 0.0f;
-
+    private float damage = 1.0f; 
+    private GameObject triggerEnemy; 
 
 	
 	
 	// Update is called once per frame
-	void Update () {
-		transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-		timePassed += 1 * Time.deltaTime; 
-		if(timePassed >= 5)
-		{
-			Destroy(this.gameObject);
-		}
-	}
+    void Update()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        timePassed += 1 * Time.deltaTime;
+        if (timePassed >= 5)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            triggerEnemy = other.gameObject;
+            triggerEnemy.GetComponent<Enemy>().health -= damage;
+            Destroy(this.gameObject);
+        }
+
+    }
 }
+   
 
